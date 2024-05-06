4a4fe11858331abd64ae1362ca68fb00a604fa88
playcsgo <sakura_0125@hotmail.com>
2024-05-06 09:44:43 +0800

    revise database and redeploy

---
+++ app.js
@@ -0,0 +1,31 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+const path = require('path')
+const express = require('express')
+const routes = require('./routes')
+const methodOverride = require('method-override')
+const app = express()
+const port = process.env.PORT || 3000
+const cors = require('cors')
+
+app.use(express.urlencoded({ extended: true }))
+app.use('/upload', express.static(path.join(__dirname, 'upload')))
+app.use(methodOverride('_method'))
+app.use(express.json())
+
+app.use(
+  cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+    preflightContinue: false
+  })
+)
+
+app.use(routes)
+app.get('/', (req, res) => res.send('Hello Kitty - Users/Tweets Done'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+module.exports = app
---
+++ migrations/20230607150738-create-user.js
@@ -0,0 +1,48 @@
+'use strict'
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      account: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      avatar: {
+        type: Sequelize.STRING
+      },
+      introduction: {
+        type: Sequelize.TEXT
+      },
+      banner: {
+        type: Sequelize.STRING
+      },
+      role: {
+        type: Sequelize.STRING
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users')
+  }
+}
